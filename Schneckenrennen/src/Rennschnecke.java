2a573d301161df1a0c7b16b7302e195b43bda3a5
hibbes <marek@czernohous.de>
2018-10-18 08:36:34 +0200

    Schnecke angelegt

--- Schneckenrennen/src/Rennschnecke.java
+++ Schneckenrennen/src/Rennschnecke.java
@@ -1,4 +1,16 @@
 
 public class Rennschnecke {
 
+	String name;
+	String rasse;
+	int vmax;
+	int strecke;
+	
+	public Rennschnecke(String name, String rasse, int vmax){
+		this.name=name;
+		this.rasse=rasse;
+		this.vmax=vmax;
+		strecke=0;
+	}
+	
 }
