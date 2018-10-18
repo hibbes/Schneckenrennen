3be5c497c2fe929d01d1af00bed8ffb6c4e07c2e
Marek.Czernohous <Marek.Czernohous@DESKTOP-JUVVB7M.2631.skool.net>
2018-10-18 08:36:34 +0200

    Schnecke angelegt

--- Schneckenrennen/src/Schneckenrennen.java
+++ Schneckenrennen/src/Schneckenrennen.java
@@ -2,7 +2,9 @@
 public class Schneckenrennen {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Rennschnecke s1 = new Rennschnecke("Hasi", "Glitscho",3);
+		Rennschnecke s2 = new Rennschnecke("Baerchen", "Rasanto",5);
 
 	}
 
