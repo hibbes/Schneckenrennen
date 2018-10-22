10754feac094e67ec654877ef8af0e9171064a5c
Marek.Czernohous <Marek.Czernohous@DESKTOP-JUVVB7M.2631.skool.net>
2018-10-22 14:46:42 +0200

    Rennen angelegt

--- Schneckenrennen/src/Main.java
+++ Schneckenrennen/src/Main.java
@@ -1,11 +1,15 @@
 
-public class Schneckenrennen {
+public class Main {
 
 	public static void main(String[] args) {
 		
 		Rennschnecke s1 = new Rennschnecke("Hasi", "Glitscho",3);
 		Rennschnecke s2 = new Rennschnecke("Baerchen", "Rasanto",5);
 
+		s1.krieche();
+		System.out.println(s1);
+		s1.krieche();
+		System.out.println(s1);
 	}
 
 }
