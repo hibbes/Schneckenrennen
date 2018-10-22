c9c2745998a9fa64ca94c6b845293eafd660c6c9
hibbes <marek@czernohous.de>
2018-10-22 15:35:52 +0200

    Rennen implementiert

--- Schneckenrennen/src/Main.java
+++ Schneckenrennen/src/Main.java
@@ -5,6 +5,12 @@ public class Main {
 		
 		Rennschnecke s1 = new Rennschnecke("Hasi", "Glitscho",3);
 		Rennschnecke s2 = new Rennschnecke("Baerchen", "Rasanto",5);
+		
+		Rennen bilsterBerg = new Rennen("Bilster Berg",12);
+		
+		bilsterBerg.addRennschnecke(s1);
+		bilsterBerg.addRennschnecke(s2);
+		
 
 		s1.krieche();
 		System.out.println(s1);
--- Schneckenrennen/src/Rennen.java
+++ Schneckenrennen/src/Rennen.java
@@ -1,4 +1,28 @@
-
+import java.util.ArrayList;
 public class Rennen {
 
+	String name;
+	int teilnehmerzahl;
+	float laenge;
+	
+	ArrayList<Rennschnecke> teilnehmer = new ArrayList<Rennschnecke>();
+	
+	public Rennen(String name, float laenge){
+		this.name=name;
+		this.laenge=laenge;
+	}
+	
+	void addRennschnecke(Rennschnecke neueSchnecke){
+		teilnehmer.add(neueSchnecke);
+	}
+	
+	
+	
+	void removeRennschnecke(Rennschnecke schnecke){
+		for(Rennschnecke schnegge : teilnehmer){
+	//		if(teilnehmer == schnecke){
+		//		schnegge.remove;
+			}
+		}
+	}
 }
